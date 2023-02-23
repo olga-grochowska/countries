c536597b5ed6a6b568a4f6d246d3679404300729
Olga Grochowska <103778400+olga-grochowska@users.noreply.github.com>
2023-02-24 00:10:09 +0100

    Added basic routing

---
+++ countries/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { ListCountries } from "./Pages/ListCountries/ListCountries";
+import { Details } from "./Pages/Details/Details";
+import { NoMatch } from "./components/NoMatch";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="list" element={<ListCountries />} />
+          <Route path="details" element={<Details />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ countries/src/Pages/ListCountries/ListCountries.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ListCountries = () => {
+  return <div>ListCountries</div>;
+};
