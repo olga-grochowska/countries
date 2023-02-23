b6cb8b9ef15dfcea416d425710ea1a60cad09fa9
Olga Grochowska <103778400+olga-grochowska@users.noreply.github.com>
2023-02-24 00:31:25 +0100

    Added fetch API

--- countries/src/Pages/ListCountries/ListCountries.jsx
+++ countries/src/Pages/ListCountries/ListCountries.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export const ListCountries = () => {
+  useEffect(() => {
+    fetch(
+      "https://restcountries.com/v2/all?fields=name,capital,flag,population,regions"
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
   return <div>ListCountries</div>;
 };
